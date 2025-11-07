675054dc99ef84009c02975ff77c7e8e92664749
olivierlamauve10-sys <olivierlamauve10@gmail.com>
2025-11-07 19:33:57 +0100

    Update app.py
    Test21

--- app.py
+++ app.py
@@ -219,7 +219,7 @@ if st.button("🚀 LANCER LE SCANNER", type="primary"):
                 # === Conditions de détection ===
                 # === Conditions de détection ===
                 ema200_up_ok = ema200.iloc[-1] > ema200.iloc[-11] and ema200.iloc[-11] > ema200.iloc[-33] and ema200.iloc[-33] > ema200.iloc[-45]    # tendance haussière
-                ema50_down_ok = ema50.iloc[-2] < ema50.iloc[-3]     # retracement
+                ema50_down_ok = ema50.iloc[-2] < ema50.iloc[-4] and ema50.iloc[-4] < ema50.iloc[-6] and ema50.iloc[-6] < ema50.iloc[-8]    # retracement
                 ema7_up_ok = last['EMA7'] > prev['EMA7']          # rebond technique
 
                 # === Validation du signal global ===
