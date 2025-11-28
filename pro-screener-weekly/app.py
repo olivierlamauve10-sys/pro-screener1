5bf3c7780464eb552ca0566d74fed29406235567
olivierlamauve10-sys <olivierlamauve10@gmail.com>
2025-11-28 16:59:14 +0100

    Update app.py

--- pro-screener-weekly/app.py
+++ pro-screener-weekly/app.py
@@ -20,7 +20,7 @@ CACHE_TTL = 3600  # = 1h cache
 # CONFIGURATION GÉNÉRALE
 # ======================================
 st.set_page_config(page_title="ProScreener Pro", layout="wide")
-st.title("📈 Sauvegarde W")
+st.title("📈 Screener W")
 
 
 # ======================================
@@ -59,19 +59,6 @@ selected_markets = st.multiselect(
 
 tickers = [t for m in selected_markets for t in markets[m]]
 st.write(f"**{len(tickers)} actions sélectionnées**")
-
-
-# ======================================
-# PARAMÈTRE : % RETRACEMENT
-# ======================================
-retracement_percent = st.slider(
-    "Retracement minimal (%) par rapport au plus haut des 252 séances",
-    min_value=5,
-    max_value=30,
-    value=10,
-    step=1,
-    help="Exemple : 10% → le cours du jour doit être au moins 10% sous le plus haut atteint sur 252 séances."
-)
 
 
 # ======================================
@@ -173,22 +160,15 @@ def check_conditions(df, retracement_percent):
         and RSI7.iloc[-1] > 30
     )
 
-    highest_52 = df["High"].tail(52).max()
-    current_price = last["Close"]
-
-    retracement_threshold = 1 - (retracement_percent / 100)
-    retracement_ok = current_price <= highest_52 * retracement_threshold
-
     return (
         # ema200_up_ok
         # and ema50_down_ok
         # and ema7_up_ok
         rsi_ok
-        # and retracement_ok
     )
 
 
-def analyze_symbol(symbol, retracement_percent):
+def analyze_symbol(symbol):
     try:
         df = get_data(symbol)
         if df is None:
@@ -439,7 +419,7 @@ if st.button("🚀 LANCER LE SCANNER", type="primary"):
 
         with ThreadPoolExecutor(max_workers=max_workers) as executor:
             futures = {
-                executor.submit(analyze_symbol, symbol, retracement_percent): symbol
+                executor.submit(analyze_symbol, symbol): symbol
                 for symbol in tickers
             }
 
