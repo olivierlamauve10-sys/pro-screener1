b96ef56e64638d7e0e9d687abe21b662bd24d52d
olivierlamauve10-sys <olivierlamauve10@gmail.com>
2025-11-28 12:27:54 +0100

    Update app.py

--- W/app.py
+++ W/app.py
@@ -82,7 +82,7 @@ def get_data(symbol):
     if df is None or df.empty or len(df) < 3:
         return None
 
-    df = df[df["Volume"] > 0]          # supprimer week-ends
+    # df = df[df["Volume"] > 0]          # supprimer week-ends
     df = df.dropna(subset=["Close"])   # supprimer lignes vides
     return df
     
