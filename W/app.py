a806b207d30a43a82f4247c753e7eafd62aae870
olivierlamauve10-sys <olivierlamauve10@gmail.com>
2025-12-08 19:54:27 +0100

    Update app.py

--- W/app.py
+++ W/app.py
@@ -183,7 +183,7 @@ def check_conditions(df, retracement_percent):
         ema200_up_ok
         # and ema50_down_ok
         # and ema7_up_ok
-        and rsi_ok
+        # and rsi_ok
         # and retracement_ok
     )
 
