2ec15b70986ed797ec59e7147c5cab18ec869ca3
olivierlamauve10-sys <olivierlamauve10@gmail.com>
2025-12-20 12:16:48 +0100

    Update app.py

--- W/app.py
+++ W/app.py
@@ -193,7 +193,7 @@ def check_conditions(df):
     # =========================
     # CONDITIONS FINALES
     # =========================
-    return rsi_ok and macd_ok
+    return rsi_ok #and macd_ok
 
 
 def classify_yf_exception(e: Exception) -> str:
