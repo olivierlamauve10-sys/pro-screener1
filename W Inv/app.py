c9842ed6efa60218180db397b0b37d15517a1d07
olivierlamauve10-sys <olivierlamauve10@gmail.com>
2026-02-04 18:14:35 +0100

    Update app.py

--- W Inv/app.py
+++ W Inv/app.py
@@ -203,7 +203,7 @@ def check_conditions(df):
     # =========================
     # CONDITIONS FINALES
     # =========================
-    return rsi_ok and rsi2_ok and volume_ok and capitalisation_ok #and macd_ok
+    return rsi_ok and rsi2_ok #and volume_ok and capitalisation_ok and macd_ok
 
 
 def classify_yf_exception(e: Exception) -> str:
