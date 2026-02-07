48176e960f8f4c6ef4d85a1a16f1c87ccd392003
olivierlamauve10-sys <olivierlamauve10@gmail.com>
2026-02-07 18:34:09 +0100

    Update app.py

--- W Inv/app.py
+++ W Inv/app.py
@@ -154,13 +154,19 @@ def check_conditions(df):
     RSI7 = df["RSI7"]
 
     rsi_ok = (
-        RSI7.iloc[-3] < 30
-        and RSI7.iloc[-2] < 30
-        and RSI7.iloc[-1] > 30
+        RSI7.iloc[-5] < 30
+        and RSI7.iloc[-4] < 30
+        and RSI7.iloc[-3] > 30
+        and RSI7.iloc[-2] > RSI7.iloc[-3]
+        and RSI7.iloc[-1] > RSI7.iloc[-2]
     ) or (
         RSI7.iloc[-4] < 30
         and RSI7.iloc[-3] < 30
         and RSI7.iloc[-2] > 30
+        and RSI7.iloc[-1] > RSI7.iloc[-2]
+    ) or (
+        RSI7.iloc[-3] < 30
+        and RSI7.iloc[-2] < 30
         and RSI7.iloc[-1] > 30
     ) or (
         RSI7.iloc[-5] > 70
