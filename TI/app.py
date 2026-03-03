a07da3636e6218672e71b529fa57f1c59232fcd8
olivierlamauve10-sys <olivierlamauve10@gmail.com>
2026-03-03 12:23:51 +0100

    Update app.py

--- TI/app.py
+++ TI/app.py
@@ -221,11 +221,11 @@ def check_conditions(df: pd.DataFrame, retracement_percent: int) -> bool:
         and ema50.iloc[-45] < ema50.iloc[-60]
     )
 
-    ema7_down1_ok = (
-        ema7.iloc[-5] < ema7.iloc[-15]
-        and ema7.iloc[-15] < ema7.iloc[-25]
-        and ema7.iloc[-25] < ema7.iloc[-35]
-    )
+    #ema7_down1_ok = (
+    #    ema7.iloc[-5] < ema7.iloc[-15]
+    #    and ema7.iloc[-15] < ema7.iloc[-25]
+    #    and ema7.iloc[-25] < ema7.iloc[-35]
+    #)
     
     ema7_up_ok = last["EMA7"] > prev["EMA7"]
     
