e19b45f1fff10931bff0433b4d1e65fb5ce4685c
olivierlamauve10-sys <olivierlamauve10@gmail.com>
2026-03-03 12:09:53 +0100

    Update app.py

--- TI/app.py
+++ TI/app.py
@@ -207,7 +207,7 @@ def check_conditions(df: pd.DataFrame, retracement_percent: int) -> bool:
         #ema200.iloc[-25] > ema200.iloc[-40]
         #and ema200.iloc[-40] > ema200.iloc[-50]
         #and ema200.iloc[-50] > ema200.iloc[-55]
-        ema200.iloc[-25] / ema200.iloc[-55] > 1.025
+        ema200.iloc[-25] / ema200.iloc[-55] > 1.015
     )
     
     ema50_down1_ok = (
@@ -240,7 +240,7 @@ def check_conditions(df: pd.DataFrame, retracement_percent: int) -> bool:
         (ema50_down1_ok or ema50_down2_ok)
         and ema200_up_ok
         and ema7_up_ok
-        #and rsi_ok
+        and rsi_ok
         #and retracement_ok
         #and signal_ok
     )
