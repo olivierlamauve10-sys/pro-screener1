168696053b251a5e83b4127ba6b2ead877f2053b
olivierlamauve10-sys <olivierlamauve10@gmail.com>
2026-03-04 12:24:08 +0100

    Update app.py

--- W/app.py
+++ W/app.py
@@ -21,8 +21,7 @@ CACHE_TTL = 3600  # = 1h cache
 # CONFIGURATION GÉNÉRALE
 # ======================================
 st.set_page_config(page_title="ProScreener Pro", layout="wide")
-st.title("📈 Screener W 
-Signaux estéthiques -> Retournement entre S et S+10, espérance 70% -> à suivre en liste W de ZoneBourse")
+st.title("📈 Screener W \nSignaux estéthiques -> Retournement entre S et S+10, espérance 70% -> à suivre en liste W de ZoneBourse")
 
 
 # ======================================
