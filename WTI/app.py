e98593509fdeae9f379f98b9b0d9145423e894c0
olivierlamauve10-sys <olivierlamauve10@gmail.com>
2026-04-16 18:12:08 +0200

    Update app.py

--- WTI/app.py
+++ WTI/app.py
@@ -159,7 +159,7 @@ def get_data(symbol: str):
     # 2) Sinon requête Yahoo (petit jitter)
     time.sleep(0.08 + 0.25 * random.random())
 
-    df = yf.Ticker(symbol).history(period="0.5y", interval="1d")
+    df = yf.Ticker(symbol).history(period="1y", interval="1d")
     # df = yf.Ticker(symbol).history(period="2y", interval="1d")
 
     if df is None or df.empty:
