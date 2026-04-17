3bc5711692a5bce37376011ee92075649550c9eb
olivierlamauve10-sys <olivierlamauve10@gmail.com>
2026-04-17 12:48:12 +0200

    Update app.py

--- W Inv/app.py
+++ W Inv/app.py
@@ -22,8 +22,10 @@ CACHE_TTL = 3600  # = 1h cache
 # ======================================
 st.set_page_config(page_title="ProScreener Pro", layout="wide")
 st.title("📈 Screener W et W Inv")
-st.title("Signaux esthétiques -> Retournement entre S et S+10, espérance 70% -> à suivre en liste W de ZoneBourse")
-help (streamlit)
+st.title("Signaux esthétiques -> Retournement entre S et S+10, espérance 70%")
+st.title("-> à suivre en liste W de ZoneBourse")
+
+
 # ======================================
 # CHARGEMENT DES MARCHÉS
 # ======================================
