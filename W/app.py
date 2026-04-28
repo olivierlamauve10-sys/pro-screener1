68781877dd6352a7ae6c8c8f2a241b8805598647
olivierlamauve10-sys <olivierlamauve10@gmail.com>
2026-04-28 14:24:05 +0200

    Update app.py

--- W/app.py
+++ W/app.py
@@ -196,7 +196,7 @@ def check_conditions(df):
     # =======================================
     
     close = df["Close"]
-    # current_price = last["Close"]
+    current_price = last["Close"]
     # ema200 = df["ema200"]
     # seuil_ok = current_price < ema200
     
