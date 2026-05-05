1bee8c55f570f2231decb768db475310b2147a0e
olivierlamauve10-sys <olivierlamauve10@gmail.com>
2026-05-05 16:15:42 +0200

    Update app.py

--- TI/app.py
+++ TI/app.py
@@ -157,7 +157,6 @@ def get_data(symbol: str):
     time.sleep(0.08 + 0.18 * random.random())
 
     df = yf.Ticker(symbol).history(period="1y", interval="1d")
-    # df = yf.Ticker(symbol).history(period="8mo", interval="1d")
     # df = yf.Ticker(symbol).history(period="2y", interval="1d")
 
     if df is None or df.empty:
